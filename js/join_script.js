59a738fdb8fe4fdf518a1386983dc0665f535a78
hackznbugz <51843650+hackznbugz@users.noreply.github.com>
2020-12-01 17:56:24 +0530

    Add files via upload

---
+++ js/join_script.js
@@ -0,0 +1,78 @@
+var name_field = document.getElementById('form_name1');
+var address_field = document.getElementById('form_name2');
+var phone_field = document.getElementById('form_name3');
+var email_field = document.getElementById('form_name4');
+
+var submit = document.getElementById('submit');
+var back = document.getElementById('back');
+
+var name = '';
+var address = '';
+var phone = '';
+var email = '';
+
+name_field.oninput = function(){
+    name = name_field.value;
+    enableSubmit();
+}
+address_field.oninput = function(){
+    address = address_field.value;
+    enableSubmit();
+}
+phone_field.oninput = function(){
+    phone = phone_field.value;
+    enableSubmit();
+}
+email_field.oninput = function(){
+    email = email_field.value;
+    enableSubmit();
+}
+
+function enableSubmit(){
+    if(name.trim() == '' || address.trim() == '' || phone.trim() == ''){
+        submit.disabled = true;
+        submit.style.opacity = .7;
+    }else{
+        submit.disabled = false;
+        submit.style.opacity = 1;
+    }
+}
+
+back.onclick = function(){
+    window.location.href = "../index.html";
+}
+
+submit.onclick = function(){
+    if(email.trim() != ''){
+        if(validateEmail(email)){
+            //something
+        }else{
+            alert('Email is not valid!')
+        }
+    }
+    if(validatePhone(phone)){
+        //something
+    }else{
+        alert('Phone is not valid!')
+    }
+}
+
+function validateEmail(mail) 
+{
+ if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(mail))
+  {
+    return (true)
+  }
+    return (false)
+}
+
+function validatePhone(inputtxt)
+{
+    var phoneno = /^\d{10}$/;
+    if(inputtxt.match(phoneno)){
+      return true;
+    }
+    else{
+        return false;
+    }
+}
