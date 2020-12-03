700f7673d119a889fd1607c3b328169b6ae2077e
hackznbugz <51843650+hackznbugz@users.noreply.github.com>
2020-12-03 17:13:04 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ var navLinks = document.getElementsByClassName('navb-mobile')[0];
 var isOpened = false;
 hamburger.addEventListener('click', ()=>{
     if(isOpened){
-        navLinks.style.transform = "translateX(100%)";
+        navLinks.style.transform = "translateX(100vw)";
         hamburgerLayers[0].style.margin = ".5em 0";
         hamburgerLayers[0].style.transform = "none";
         hamburgerLayers[1].style.display = "block";
@@ -24,11 +24,7 @@ hamburger.addEventListener('click', ()=>{
     }
 });
 
-window.addEventListener('resize', function(){
-    document.getElementsByClassName('main')[0].style.height = document.getElementsByClassName('mainImage')[0].height + 'px';
-});
 
-document.getElementsByClassName('main')[0].style.height = document.getElementsByClassName('mainImage')[0].height + 'px';
 
 //scroll
 $(document).ready(function() {
@@ -63,3 +59,43 @@ for(i=0; i<linksMobile.length; i++){
         $( "#btn" ).click();
     }
 }
+
+
+function getViewport() {
+
+    var viewPortWidth;
+    var viewPortHeight;
+   
+    // the more standards compliant browsers (mozilla/netscape/opera/IE7) use window.innerWidth and window.innerHeight
+    if (typeof window.innerWidth != 'undefined') {
+      viewPortWidth = window.innerWidth,
+      viewPortHeight = window.innerHeight
+    }
+   
+   // IE6 in standards compliant mode (i.e. with a valid doctype as the first line in the document)
+    else if (typeof document.documentElement != 'undefined'
+    && typeof document.documentElement.clientWidth !=
+    'undefined' && document.documentElement.clientWidth != 0) {
+       viewPortWidth = document.documentElement.clientWidth,
+       viewPortHeight = document.documentElement.clientHeight
+    }
+   
+    // older versions of IE
+    else {
+      viewPortWidth = document.getElementsByTagName('body')[0].clientWidth,
+      viewPortHeight = document.getElementsByTagName('body')[0].clientHeight
+    }
+    return [viewPortWidth, viewPortHeight];
+}
+
+
+function resize(){
+    var viewPort = getViewport();
+    if(viewPort[0]<580){
+        document.getElementsByClassName('main')[0].style.height = document.getElementsByClassName('mainImage')[0].height + 'px';
+    }
+}
+
+window.addEventListener('resize', function(){
+    resize();
+});
