a4257bfcbdaa51f6bbdedc42b14d4d9951d6abb9
hackznbugz <51843650+hackznbugz@users.noreply.github.com>
2020-12-14 12:51:55 +0530

    Update join_script.js

--- js/join_script.js
+++ js/join_script.js
@@ -1,7 +1,23 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDWg4mns_IEl4M9_xhXOgBb_xPyuxoOQNU",
+    authDomain: "manee-fc4b7.firebaseapp.com",
+    projectId: "manee-fc4b7",
+    storageBucket: "manee-fc4b7.appspot.com",
+    messagingSenderId: "709101093528",
+    appId: "1:709101093528:web:660439befbc8221cdcc6ed",
+    measurementId: "G-938SD0WB39",
+    databaseURL: "https://manee-fc4b7-default-rtdb.firebaseio.com/",
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+var database = firebase.database();
+
 var name_field = document.getElementById('form_name1');
 var address_field = document.getElementById('form_name2');
 var phone_field = document.getElementById('form_name3');
 var email_field = document.getElementById('form_name4');
+var des_field = document.getElementById('form_name5');
 
 var submit = document.getElementById('submit');
 var back = document.getElementById('back');
@@ -10,6 +26,7 @@ var name = '';
 var address = '';
 var phone = '';
 var email = '';
+var des = '';
 
 name_field.oninput = function(){
     name = name_field.value;
@@ -27,9 +44,13 @@ email_field.oninput = function(){
     email = email_field.value;
     enableSubmit();
 }
+des_field.onchange = function(){
+    des = des_field.value;
+    enableSubmit();
+}
 
 function enableSubmit(){
-    if(name.trim() == '' || address.trim() == '' || phone.trim() == ''){
+    if(name.trim() == '' || address.trim() == '' || phone.trim() == '' || des.trim() == '' || email.trim() == ''){
         submit.disabled = true;
         submit.style.opacity = .7;
     }else{
@@ -44,16 +65,22 @@ back.onclick = function(){
 
 submit.onclick = function(){
     if(validatePhone(phone)){
-        if(email.trim() != ''){
-            if(validateEmail(email)){
-                document.getElementsByClassName('form-md')[0].style.display = 'none';
-                document.getElementById('h1').innerHTML = 'Thank You!';
-            }else{
-                alert('Email is not valid!')
-            }
-        }else{
+        if(validateEmail(email)){
             document.getElementsByClassName('form-md')[0].style.display = 'none';
-            document.getElementById('h1').innerHTML = 'Thank You!';
+            document.getElementById('h1').innerHTML = 'Thank You! <br><span style="font-size:27px;text-align:center;">for expressing your desire to learn</span>';
+            document.getElementById('h1').style.lineHeight = '50px';
+            document.getElementById('h1').style.textAlign = 'center';
+            document.getElementById('h1').style.padding = '5rem';
+
+            firebase.database().ref().push({
+                name: name,
+                address: address,
+                phone: phone,
+                email: email,
+                designation : des
+            });
+        }else{
+            alert('Email is not valid!')
         }
     }else{
         alert('Phone is not valid!')
